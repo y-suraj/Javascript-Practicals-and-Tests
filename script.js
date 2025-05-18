013fdb60aac99ce2c382849e295192e46b0545b7
y-suraj <76870806+y-suraj@users.noreply.github.com>
2025-05-18 16:51:13 +0530

    Manipulating DOM With JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // get quote from API
 async function getQuote() {
     // Fetch a random quote from the Forismatic API
@@ -10,13 +16,32 @@ async function getQuote() {
         const response = await fetch(proxyUrl + apiUrl);
         // Check if the response is ok (status code 200)
         const data = await response.json();
-        // Check if the data is valid
-        console.log(data);
+        authorText.innerText = data.quoteAuthor || 'Anonymous'; // If no author, set to 'Anonymous'
+        quoteText.innerText = data.quoteText; // Set the quote text
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
     } catch (error) {
         getQuote(); // Try again if there is an error
-        console.log('Error, no quote', error);
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    // Create a Twitter share link with the quote and author
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}%0A- ${author}`;
+    // Open the Twitter share link in a new tab
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', getQuote); // Get a new quote when the button is clicked
+twitterBtn.addEventListener('click', tweetQuote); // Tweet the quote when the button is clicked
 
 // On load, get a new quote
 getQuote();
